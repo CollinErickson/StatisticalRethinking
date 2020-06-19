de64e3daeaea61fec8da47b9be7b7adbd172502f
Collin Erickson <collinberickson@gmail.com>
2020-06-18 22:15:30 -0400

    worked on ch 11

--- chapter11problems.R
+++ chapter11problems.R
@@ -4,6 +4,9 @@ library(dplyr)
 library(magrittr)
 library(ggplot2)
 
+# Problems are for Chapter 11 (version 2),
+# but are listed as 10xx.
+
 # 10E1
 curve(logit)
 logit(.35)
@@ -16,3 +19,8 @@ inv_logit(3.2)
 
 # 10E3
 
+# 10E4
+# An offset is used to convert count to rate.
+# In the monk example, the manuscripts are counted over different time periods,
+# by day and by week. To put them on a comparable scale, we need an set to
+# correct for this time difference.
