702554414c3707780939876d27908572e3ea6eb4
Collin Erickson <collinberickson@gmail.com>
2020-07-03 20:57:46 -0400

    ch 16

---
+++ chapter16problems.R
@@ -0,0 +1,17 @@
+# Chapter 16
+library(rethinking)
+library(dplyr)
+library(magrittr)
+library(ggplot2)
+
+# 16E1
+# Disadvatanges of GLMs
+# Not everything works using linear model underneath.
+# If we have a causal model, such as based on partial differential equation,
+# we should use that instead.
+
+# 16E2
+# Other things based on differential equations:
+# rocket equation, population dynamics, quantum models
+
+# 16E3
